32ab2ffbca6f763220764c106d40b960970004a9
Andres Faraone <andresfaraone@outlook.com>
2018-11-05 16:22:11 +0000

    display has injected transactionlog list on initialize

--- lib/display.rb
+++ lib/display.rb
@@ -1,2 +1,8 @@
 class Display
+  attr_reader :list, :log
+
+  def initialize(transaction_log = TransactionLog.new)
+    @log = transaction_log
+    @list = transaction_log.list
+  end
 end
--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -1,5 +1,12 @@
 require 'display'
 
 describe Display do
+  let(:mock_transaction_log) { double(:mock_transaction_log, list: []) }
+  subject { described_class.new(mock_transaction_log)}
 
+  describe 'initialize' do
+    it 'has transaction_log list dependency injection' do
+      expect(subject.list).to eq mock_transaction_log.list
+    end
+  end
 end
