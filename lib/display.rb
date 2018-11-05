21a6b6765d1e7bf02be96686655aeaf31c659be0
Andres Faraone <andresfaraone@outlook.com>
2018-11-05 17:25:00 +0000

    format_row takes a log hash and converts to formatted string

--- lib/display.rb
+++ lib/display.rb
@@ -8,7 +8,19 @@ class Display
     @sep = DEFAULT_SEPARATOR
   end
 
+
+
   def format_time(date)
     date.strftime("%d/%m/%Y")
+  end
+
+  #does this change translog list?
+  def format_row(row)
+    output = []
+    [:date, :credit, :debit, :balance].each do |col|
+      row[col] = format_time(row[col]) if col == :date 
+      output << row[col]
+    end
+    output.join(sep) + "\n"
   end
 end
--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -1,8 +1,14 @@
 require 'display'
-
+# maybe dont use time now here? should just set a time date myself
 describe Display do
+  let(:date) { Time.parse('2018-09-10') }
   let(:mock_transaction_log) { double(:mock_transaction_log, list: []) }
+
   subject { described_class.new(mock_transaction_log)}
+
+  before do
+    allow(Time).to receive(:now).and_return(date)
+  end
 
   describe 'initialize' do
     it 'has transaction_log list dependency injection' do
@@ -11,17 +17,15 @@ describe Display do
   end
 
   describe 'format_row' do
+    let(:row) { {date: Time.now, credit: 200, balance: 1000 } }
+
     it 'converts a record hash to formatted string with separator' do
+      output = "10/09/2018 || 200 ||  || 1000\n"
+      expect(subject.format_row(row)).to eq output
     end
   end
 
   describe 'Time format' do
-    let(:date) { Time.parse('2018-09-10') }
-
-    before do
-      allow(Time).to receive(:now).and_return(date)
-    end
-
     it 'returns time in user-friendly format' do
       expect(subject.format_time(date)).to eq '10/09/2018'
     end
