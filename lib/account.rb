8c5ca04d758dd1774c640fd0a19580eba1145f34
Andres Faraone <andresfaraone@outlook.com>
2018-11-05 11:54:22 +0000

    #deposit adds argument to balance, made attr_writer balance private

--- lib/account.rb
+++ lib/account.rb
@@ -5,4 +5,11 @@ class Account
     @balance = 0
     @transaction = transaction
   end
+
+  def deposit(amount)
+    self.balance += amount
+  end
+
+  private
+  attr_writer :balance
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -13,4 +13,13 @@ describe Account do
       expect(subject.transaction).to eq mock_transaction
     end
   end
+
+  describe '#deposit' do
+    it { is_expected.to respond_to(:deposit).with(1).argument}
+
+    it 'increases @balance by argument' do
+      subject.deposit 1000
+      expect(subject.balance).to eq 1000
+    end
+  end
 end
