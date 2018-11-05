eac2780cb676a4ca64031896dc3b34b983beac03
Andres Faraone <andresfaraone@outlook.com>
2018-11-05 11:47:55 +0000

    #transaction returns injected Transaction obj

--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,8 @@
 class Account
-  attr_reader :balance
+  attr_reader :balance, :transaction
 
-  def initialize
-    @balance = 0.00
+  def initialize(transaction = Transaction.new)
+    @balance = 0
+    @transaction = transaction
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,9 +1,16 @@
 require 'account'
 
 describe Account do
+  let(:mock_transaction) { double(:mock_transaction) }
+  subject { described_class.new(mock_transaction) }
+
   describe 'initialization' do
     it 'has balance variable set to 0' do
-      expect(subject.balance).to eq 0.00
+      expect(subject.balance).to eq 0
+    end
+
+    it 'has an injected transaction obj' do
+      expect(subject.transaction).to eq mock_transaction
     end
   end
 end
