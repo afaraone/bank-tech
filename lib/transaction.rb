bce56a414d997df6b409c20c0b6f6a6eb84293c0
Andres Faraone <andresfaraone@outlook.com>
2018-11-05 14:00:56 +0000

    add record_deposit method to store deposit info in a hash

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -4,4 +4,8 @@ class Transaction
   def initialize
     @list = []
   end
+
+  def record_deposit(amount, balance)
+    { date: Time.now, credit: amount, debit: nil, balance: balance }
+  end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -6,4 +6,14 @@ describe Transaction do
       expect(subject.list).to eq []
     end
   end
+
+  describe 'record_deposit' do
+    it { is_expected.to respond_to(:record_deposit).with(2).arguments }
+
+    it 'returns a hash with amount, balance, type of transaction, date' do
+      date = Timecop.freeze(Time.new(2018, 9, 9))
+      log = { date: date, credit: 400, debit: nil, balance: 1000 }
+      expect(subject.record_deposit(400, 1000)).to eq log
+    end
+  end
 end
