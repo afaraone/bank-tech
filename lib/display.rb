24223158bc71402e8bb31a50aa1e384b197373bf
Andres Faraone <andresfaraone@outlook.com>
2018-11-07 13:24:30 +0000

    Move format logic to display class
    format_row and format_number now does not affect
    translog @list.

--- lib/display.rb
+++ lib/display.rb
@@ -10,10 +10,21 @@ class Display
   private
 
   def header
-    "date || credit || debit || balance"
+    'date || credit || debit || balance'
   end
 
   def format_row(row)
-    "#{row[:date]} || #{row[:credit]} || #{row[:debit]} || #{row[:balance]}".squeeze(' ')
+    "#{format_time(row[:date])} || #{format_number(row[:credit])} || "\
+    "#{format_number(row[:debit])} || #{format_number(row[:balance])}".squeeze(' ')
+  end
+
+  def format_number(number)
+    format('%.2f', number)
+  rescue StandardError
+    nil
+  end
+
+  def format_time(date)
+    date.strftime('%d/%m/%Y')
   end
 end
--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -3,16 +3,20 @@
 require 'display'
 describe Display do
   describe '#statement' do
-    let(:row) { { date: '15/09/2018', credit: 200, balance: 1700 } }
-    let(:row2) { { date: '12/09/2018', credit: 500, balance: 1500 } }
-    let(:row3) { { date: '10/09/2018', debit: 200, balance: 1000 } }
+    let(:date) { Time.parse('2018-09-15') }
+    let(:date2) { Time.parse('2018-09-12') }
+    let(:date3) { Time.parse('2018-09-10') }
+
+    let(:row) { { date: date, credit: 200.00, balance: 1700.00 } }
+    let(:row2) { { date: date2, credit: 500.00, balance: 1500.00 } }
+    let(:row3) { { date: date3, debit: 200.00, balance: 1000.00 } }
     let(:list) { [row, row2, row3] }
 
     let(:statement) do
       ['date || credit || debit || balance',
-       '15/09/2018 || 200 || || 1700',
-       '12/09/2018 || 500 || || 1500',
-       '10/09/2018 || || 200 || 1000'].join("\n") + "\n"
+       '15/09/2018 || 200.00 || || 1700.00',
+       '12/09/2018 || 500.00 || || 1500.00',
+       '10/09/2018 || || 200.00 || 1000.00'].join("\n") + "\n"
     end
 
     it 'prints header row and full list of transactions' do
